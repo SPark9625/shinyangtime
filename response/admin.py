b5ec97ecc53535e22c345e9adabc45b881d11ce5
Semin Park <SPark9625@gmail.com>
2017-10-25 15:59:23 +0900

    hide weekday, start, end

--- response/admin.py
+++ response/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from . import models
 
 class TimeTableAdmin(admin.ModelAdmin):
+	
 	fieldsets = [
 		(None, 				{"fields": ["default"]}),
 		("Year/Semester", 	{"fields": ["year", "semester"]}),
-		("Period related", 	{"fields": ["date", "weekday", "period", "start", "end"]}),
+		("Period related", 	{"fields": ["date", "period"]}),
 		("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
 	]
 
