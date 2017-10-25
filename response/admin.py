17bb957f2320d0aa364f127cd0b3ee4475c1faa2
Semin Park <SPark9625@gmail.com>
2017-10-25 16:40:33 +0900

    admin fields change

--- response/admin.py
+++ response/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from . import models
 
 class TimeTableAdmin(admin.ModelAdmin):
-	
+	fields = ["year","semester","grade","division","subject","teacher","date","period"]
 	fieldsets = [
-		(None, 				{"fields": ["default"]}),
+		# (None, 				{"fields": ["default"]}),
 		("Year/Semester", 	{"fields": ["year", "semester"]}),
 		("Period related", 	{"fields": ["date", "period"]}),
 		("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
