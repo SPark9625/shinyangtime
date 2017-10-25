47b4575c0429a062fb81cdf6ed3082fdde12faf1
Semin Park <SPark9625@gmail.com>
2017-10-25 18:42:30 +0900

    almost fixed

--- response/period_to_time.py
+++ response/period_to_time.py
@@ -33,17 +33,17 @@ class Base:
 
 class Custom:
 	def start_2017_Fall_13(instance):
-	if instance.period == 1:
-		return "10:00"
-	elif instance.period == 2:
-		return "10:55"
-	elif instance.period == 3:
-		return "11:50"
-	elif instance.period == 4:
-		return "12:45"
-	elif instance.period == 5:
-		return "14:15"
-	elif instance.period == 6:
-		return "15:10"
-	elif instance.period == 7:
-		return "16:05"
+		if instance.period == 1:
+			return "10:00"
+		elif instance.period == 2:
+			return "10:55"
+		elif instance.period == 3:
+			return "11:50"
+		elif instance.period == 4:
+			return "12:45"
+		elif instance.period == 5:
+			return "14:15"
+		elif instance.period == 6:
+			return "15:10"
+		elif instance.period == 7:
+			return "16:05"
