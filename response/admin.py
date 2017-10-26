ac4e8f827dca14b118c97a56aaf18a98f0d169ba
Semin Park <SPark9625@gmail.com>
2017-10-26 10:45:42 +0900

    show all fields

--- response/admin.py
+++ response/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from . import models
 
 class TimeTableAdmin(admin.ModelAdmin):
-	
-	fieldsets = [
-		# (None, 				{"fields": ["default"]}),
-		# ("Year/Semester", 	{"fields": ["year", "semester"]}),
-		("Period related", 	{"fields": ["date", "period"]}),
-		("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
-	]
+	pass
+	# fieldsets = [
+	# 	# (None, 				{"fields": ["default"]}),
+	# 	# ("Year/Semester", 	{"fields": ["year", "semester"]}),
+	# 	("Period related", 	{"fields": ["date", "period"]}),
+	# 	("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
+	# ]
 
 admin.site.register(models.TimeTable, TimeTableAdmin)
