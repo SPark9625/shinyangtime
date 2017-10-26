9b5e8588725b127551cce3b17e240f61f16e9317
Semin Park <SPark9625@gmail.com>
2017-10-26 22:46:21 +0900

    modified column

--- response/admin.py
+++ response/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from . import models
 
 class TimeTableAdmin(admin.ModelAdmin):
-	list_filter = ["year","semester","date","grade","division"]
-	# fieldsets = [
-	# 	# (None, 				{"fields": ["default"]}),
-	# 	# ("Year/Semester", 	{"fields": ["year", "semester"]}),
-	# 	("Period related", 	{"fields": ["date", "period"]}),
-	# 	("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
-	# ]
+	list_filter = ["default","year","semester","date","grade","division"]
+	fieldsets = [
+		# (None, 				{"fields": ["default"]}),
+		# ("Year/Semester", 	{"fields": ["year", "semester"]}),
+		("Period related", 	{"fields": ["date", "period"]}),
+		("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
+	]
 
 admin.site.register(models.TimeTable, TimeTableAdmin)
