5c268d8991c5f1b4699c5adefbf68b20bedd67b1
Semin Park <SPark9625@gmail.com>
2017-12-18 10:14:29 +0900

    easier period to time

--- response/tools/period_to_time.py
+++ response/tools/period_to_time.py
@@ -1,97 +1,78 @@
+def save(instance, start_end_list):
+	p = instance.period - 1
+	instance.start, instance.end = start_end_list[p]
+	instance.save()
+
 class Base:
-	def start_end(instance):
-		if instance.period == 1:
-			return ["09:15", "10:00"]
-		elif instance.period == 2:
-			return ["10:10", "10:55"]
-		elif instance.period == 3:
-			return ["11:05", "11:50"]
-		elif instance.period == 4:
-			return ["12:00", "12:45"]
-		elif instance.period == 5:
-			return ["13:30", "14:15"]
-		elif instance.period == 6:
-			return ["14:25", "15:10"]
-		elif instance.period == 7:
-			return ["15:20", "16:05"]
+	def base(instance):
+		start_end_list = [
+					("09:15", "10:00"),
+					("10:10", "10:55"),
+					("11:05", "11:50"),
+					("12:00", "12:45"),
+					("13:30", "14:15"),
+					("14:25", "15:10"),
+					("15:20", "16:05"),
+					]
+		save(instance, start_end_list)
 
 class Custom:
-	def start_end_2017_10_27(instance):
-		if instance.period == 1:
-			return ["09:10", "09:40"]
-		elif instance.period == 2:
-			return ["09:50", "10:20"]
-		elif instance.period == 3:
-			return ["10:30", "11:00"]
-		elif instance.period == 4:
-			return ["11:10", "11:40"]
-		elif instance.period == 5:
-			return ["11:50", "12:20"]
-		elif instance.period == 6:
-			return ["13:25", "13:55"]
-		elif instance.period == 7:
-			return ["00:00", "00:00"]
+	def type1(instance):
+		start_end_list = [
+					("09:10", "09:40"),  # 30m
+					("09:50", "10:20"),  # 30m
+					("10:30", "11:00"),  # 30m
+					("11:10", "11:40"),  # 30m
+					("11:50", "12:20"),  # 30m
+					("13:25", "13:55"),  # 30m
+					("00:00", "00:00"),
+					]
+		save(instance, start_end_list)
 
-	def start_end_2017_10_31(instance):
-		if instance.period == 1:
-			return ["09:15", "09:55"]
-		elif instance.period == 2:
-			return ["10:05", "10:45"]
-		elif instance.period == 3:
-			return ["10:55", "11:35"]
-		elif instance.period == 4:
-			return ["11:45", "12:25"]
-		elif instance.period == 5:
-			return ["13:10", "13:45"]
-		elif instance.period == 6:
-			return ["13:55", "14:30"]
-		elif instance.period == 7:
-			return ["14:40", "15:15"]
+	def type2(instance):
+		start_end_list = [
+					("09:15", "09:55"),  # 40m
+					("10:05", "10:45"),  # 40m
+					("10:55", "11:35"),  # 40m
+					("11:45", "12:25"),  # 40m
+					("13:10", "13:45"),  # 35m
+					("13:55", "14:30"),  # 35m
+					("14:40", "15:15"),  # 35m
+					]
+		save(instance, start_end_list)
 
-	def start_end_2017_11_15(instance):
-		if instance.period == 1:
-			return ["09:15", "09:50"]
-		elif instance.period == 2:
-			return ["10:00", "10:35"]
-		elif instance.period == 3:
-			return ["10:45", "11:20"]
-		elif instance.period == 4:
-			return ["11:30", "12:05"]
-		elif instance.period == 5:
-			return ["12:15", "12:50"]
-		elif instance.period == 6:
-			return ["13:35", "14:20"]
-		elif instance.period == 7:
-			return ["14:30", "15:15"]
+	def type3(instance):
+		start_end_list = [
+					("09:15", "09:50"),  # 35m
+					("10:00", "10:35"),  # 35m
+					("10:45", "11:20"),  # 35m
+					("11:30", "12:05"),  # 35m
+					("12:15", "12:50"),  # 35m
+					("13:35", "14:20"),  # 45m
+					("14:30", "15:15"),  # 45m
+					]
+		save(instance, start_end_list)
 
-	def start_end_2017_11_22(instance):
-		if instance.period == 1:
-			return ["09:15", "09:50"]
-		elif instance.period == 2:
-			return ["10:00", "10:35"]
-		elif instance.period == 3:
-			return ["10:45", "11:20"]
-		elif instance.period == 4:
-			return ["11:30", "12:05"]
-		elif instance.period == 5:
-			return ["12:15", "12:50"]
-		elif instance.period == 6:
-			return ["13:35", "14:20"]
-		elif instance.period == 7:
-			return ["14:30", "15:15"]
+	def type4(instance):
+		start_end_list = [
+					("09:15", "09:45"),  # 30m
+					("09:55", "10:25"),  # 30m
+					("10:35", "11:05"),  # 30m
+					("11:15", "11:45"),  # 30m
+					("11:55", "12:25"),  # 30m
+					("13:10", "13:40"),  # 30m
+					("13:50", "14:20"),  # 30m
+					]
+		save(instance, start_end_list)
 
-	def start_end_2017_12_11(instance):
-		if instance.period == 1:
-			return ["09:15", "09:45"]
-		elif instance.period == 2:
-			return ["09:55", "10:25"]
-		elif instance.period == 3:
-			return ["10:35", "11:05"]
-		elif instance.period == 4:
-			return ["11:15", "11:45"]
-		elif instance.period == 5:
-			return ["11:55", "12:25"]
-		elif instance.period == 6:
-			return ["13:10", "13:40"]
-		elif instance.period == 7:
-			return ["13:50", "14:20"]
+	def type5(instance):
+				start_end_list = [
+					("09:15", "09:55"),  # 40m
+					("10:05", "10:45"),  # 40m
+					("10:55", "11:35"),  # 40m
+					("11:45", "12:25"),  # 40m
+					("13:10", "13:50"),  # 40m
+					("14:00", "14:40"),  # 40m
+					("14:50", "15:30"),  # 40m
+					]
+		save(instance, start_end_list)
