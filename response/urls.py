cbb2424a7a83a212c3169d7a2873e6bf4f4a238f
Semin Park <SPark9625@gmail.com>
2018-03-19 12:36:07 +0900

    ...

--- response/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from response import views
-
-app_name = "response"
-urlpatterns = [
-	url(r"^keyboard/", views.keyboard, name="keyboard"),
-	url(r"^message", views.answer, name="answer")
-]
--- semin.py
+++
@@ -1,3 +0,0 @@
-exec('from response.tools.period_to_time import *')
-exec('from response.tools.timetable import *')
-exec('from response.models import *')
