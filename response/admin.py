d8020951138c1347de4e8f8bd64979ca567b5f96
Semin Park <SPark9625@gmail.com>
2018-03-19 09:42:45 +0900

    ...

--- response/admin.py
+++ response/admin.py
@@ -4,8 +4,9 @@ from response import models
 class TimeTableAdmin(admin.ModelAdmin):
 	list_filter = ["default","modified","year","semester","date","grade","division"]
 	fieldsets = [
-		(None, 				{"fields": ["modified"]}),
-		("Year/Semester", 	{"fields": ["year", "semester"]}),
+		# (None, 				{"fields": ["modified"]}), #default 시간표 넣은 후 복구
+		
+		# ("Year/Semester", 	{"fields": ["year", "semester"]}),
 		("Period related", 	{"fields": ["date", "period"]}),
 		("Class related", 	{"fields": ["subject", "teacher", "grade", "division"]}),
 	]
